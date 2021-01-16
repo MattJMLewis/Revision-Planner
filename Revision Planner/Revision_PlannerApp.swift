b686e7da32d05277ab50d04d1bcafb61b47a8936
Matt-JM-Lewis <67329332+Matt-JM-Lewis@users.noreply.github.com>
2021-01-16 18:14:47 +0000

    Initial Commit

---
+++ Revision Planner/Revision_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Revision_PlannerApp.swift
+//  Revision Planner
+//
+//  Created by Matthew Lewis on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Revision_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
